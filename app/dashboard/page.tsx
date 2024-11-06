123b12a6eb19197af4d627c1fb7144ab9d5c7842
Parkhyunseung0615 <hshj0615@gmail.com>
2024-11-06 09:45:25 +0900

    환경변수 추가

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,6 +1,7 @@
 import { Card } from '@/app/ui/dashboard/cards';
 import { lusitana } from '@/app/ui/fonts';
 import { fetchCardData } from '@/app/lib/data';
+import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 
 export default async function Dashboard() {
   const {
